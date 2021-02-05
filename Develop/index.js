858c3130d7401d26c8b964b7795935052d549bd7
Sarah Finck <sarahfinck95@gmail.com>
2021-02-05 15:26:25 -0600

    created questions object, created a functionto write README file, created a function to intialize the app

--- Develop/index.js
+++ Develop/index.js
@@ -1,15 +1,72 @@
 // TODO: Include packages needed for this application
-const inquirer = require('inquier');
+const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name: 'projectTitle',
+    },
+    {
+        type: 'input',
+        message: 'Describe your project',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Enter your installation instructions here',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'Enter your usage information here',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'Enter your contribution guidelines here',
+        name: 'contribution',
+    },
+    {
+        type: 'input',
+        message: 'Enter your test instructions here',
+        name: 'test',
+    },
+    {
+        type: 'list',
+        message: 'Choose a license for your application',
+        name: 'license',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        message: 'Enter your GitHub username',
+        name: 'username',
+    },
+    {
+        type: 'input',
+        message: 'Enter your email address',
+        name: 'email',
+    },];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((data) =>{
+            writeToFile('README.md', generateMarkdown(data))
+        })
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,12 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+// choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+function renderLicenseBadge(license) {
+  if (license === 'MIT'){
+
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -13,7 +19,6 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
 `;
 }
 
