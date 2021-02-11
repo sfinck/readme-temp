2821bda6ea515a42406d2de82fa331bfaa34f3e8
Sarah Finck <sarahfinck95@gmail.com>
2021-02-11 17:26:22 -0600

    added git hub username link into questions section in read me, added table of contents, reworded headings of readme

---
+++ Develop/README.md
@@ -0,0 +1,26 @@
+# Orange
+  ![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)
+  ## Description
+  Apple
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
+  ## Installation 
+  Blah blah blah
+  ## Usage
+  isjfeis
+  ## Contributing
+  sifjsijf
+  ## Tests
+  iefjiefj
+  ## License 
+    APACHE 2.0
+    https://opensource.org/licenses/Apache-2.0
+  ## Questions
+  GitHub Username: https://github.com/sfinck
+
+  If you have any additional questions, please contact me at: sarahfinck95@gmail.com
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -2,16 +2,16 @@
 // If there is no license, return an empty string
 
 function renderLicenseBadge(license) {
-  if (license === 'MIT'){
+  if (license === 'MIT') {
     return `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)`
   }
-  else if (license === 'APACHE 2.0'){
+  else if (license === 'APACHE 2.0') {
     return `![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)`
   }
-  else if (license === 'GPL 3.0'){
+  else if (license === 'GPL 3.0') {
     return `![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)`
   }
-  else if (license === 'BSD 3'){
+  else if (license === 'BSD 3') {
     return `![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)`
   }
   else {
@@ -22,16 +22,16 @@ function renderLicenseBadge(license) {
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
-  if (license === 'MIT'){
+  if (license === 'MIT') {
     return `https://opensource.org/licenses/MIT`
   }
-  else if (license === 'APACHE 2.0'){
+  else if (license === 'APACHE 2.0') {
     return `https://opensource.org/licenses/Apache-2.0`
   }
-  else if (license === 'GPL 3.0'){
+  else if (license === 'GPL 3.0') {
     return `https://www.gnu.org/licenses/gpl-3.0`
   }
-  else if (license === 'BSD 3'){
+  else if (license === 'BSD 3') {
     return `https://opensource.org/licenses/BSD-3-Clause`
   }
   else {
@@ -42,7 +42,7 @@ function renderLicenseLink(license) {
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
-  if (license !== 'None'){
+  if (license !== 'None') {
     return `## License 
     ${license}
     ${renderLicenseLink(license)}`
@@ -58,18 +58,25 @@ function generateMarkdown(data) {
   ${renderLicenseBadge(data.license)}
   ## Description
   ${data.description}
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [License](#license)
+  * [Questions](#questions)
   ## Installation 
   ${data.installation}
   ## Usage
   ${data.usage}
-  ## Contribution
+  ## Contributing
   ${data.contribution}
-  ## Test
+  ## Tests
   ${data.test}
   ${renderLicenseSection(data.license)}
-  ## Contact Me 
-  GitHub Username: ${data.username}\n
-  Email: ${data.email}`;
+  ## Questions
+  GitHub Username: https://github.com/${data.username}\n
+  If you have any additional questions, please contact me at: ${data.email}`;
 }
 
 module.exports = generateMarkdown;
